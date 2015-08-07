58fa9059837f0ece3340d5cad2b99fca5a3adba0
applenele <1941973283@qq.com>
2015-08-07 13:16:24 +0800

    1

---
+++ JFYInformation/JFYInformation/Models/Common.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JFYInformation.Models
+{
+
+    public enum CompanyStatu { Untreated, Lock, Treated }
+    public class CommonDisply
+    {
+        public string[] CompanyStatuDisply = new string[] { "未处理", "锁定", "处理" };
+    }
+}
--- JFYInformation/JFYInformation/Models/Company.cs
+++ JFYInformation/JFYInformation/Models/Company.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JFYInformation.Models
 {
+
+    /// <summary>
+    /// 公司实体
+    /// </summary>
     public class Company
     {
         public int ID { get; set; }
 
         public string CompanyName { get; set; }
 
-        public string City { get; set; }
+        public string Address { get; set; }
 
         /// <summary>
         /// 联系人
@@ -23,11 +28,43 @@ namespace JFYInformation.Models
 
         public string Description { set; get; }
 
-
+        /// <summary>
+        /// 行业
+        /// </summary>
         public string Industry { set; get; }
 
+        /// <summary>
+        ///  URL
+        /// </summary>
         public string URL { set; get; }
 
-        public DateTime Time { get; set; }
+        /// <summary>
+        /// 规模
+        /// </summary>
+        [StringLength(100)]
+        public string Scale { set; get; }
+
+        /// <summary>
+        /// 性质
+        /// </summary>
+        [StringLength(100)]
+        public string Property { set; get; }
+
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public DateTime? Time { get; set; }
+
+
+        public int StatuAsInt { set; get; }
+
+        public string Source { set; get; }
+
+        public CompanyStatu CompanyStatu
+        {
+            set { StatuAsInt = (int)value; }
+            get { return (CompanyStatu)StatuAsInt; }
+        }
+
     }
 }
