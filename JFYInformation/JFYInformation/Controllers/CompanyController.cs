726237ec7ad8f2e36d9fd0533f4e89afbfacab1e
applenele <1941973283@qq.com>
2015-08-07 14:55:11 +0800

    2

---
+++ JFYInformation/JFYInformation/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using JFYInformation.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JFYInformation.Controllers
+{
+    public class CompanyController : BaseController
+    {
+        // GET: Company
+        public ActionResult Index(int p = 0)
+        {
+            var query = db.Companies.AsEnumerable();
+
+            query = query.OrderByDescending(x => x.Time);
+            ViewBag.PageInfo = PagerHelper.Do(ref query, 20, p);
+            return View(query);
+        }
+    }
+}
