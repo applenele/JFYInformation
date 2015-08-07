e982638e1725394e44e1a370c2687e8a6143a94e
applenele <1941973283@qq.com>
2015-08-07 16:03:10 +0800

    vcompany

--- JFYInformation/JFYInformation/Models/ViewModel/vCompany.cs
+++ JFYInformation/JFYInformation/Models/ViewModel/vCompany.cs
@@ -12,6 +12,49 @@ namespace JFYInformation.Models.ViewModel
 
         public string CompanyName { get; set; }
 
-        public int MyProperty { get; set; }
+        public string Address { get; set; }
+
+        public string Contacts { set; get; }
+
+        public string Phone { set; get; }
+
+        public string Description { set; get; }
+
+        public string Industry { set; get; }
+
+        public string URL { set; get; }
+
+        public string Scale { set; get; }
+
+        public string Property { set; get; }
+
+
+        public DateTime? Time { set; get; }
+
+        public string Statu { set; get; }
+
+
+        public string Source { set; get; }
+
+        public string DealResult { set; get; }
+
+
+        public vCompany() { }
+
+        public vCompany(Company model)
+        {
+            this.ID = model.ID;
+            this.CompanyName = model.CompanyName;
+            this.Address = model.Address;
+            this.Contacts = model.Contacts;
+            this.Phone = model.Phone;
+            this.Description = model.Description;
+            this.Industry = model.Industry;
+            this.URL = model.URL;
+            this.Property = model.Property;
+            this.Time = model.Time;
+            this.Statu = CommonDisply.CompanyStatuDisply[model.StatuAsInt];
+            this.DealResult = CommonDisply.DealResultDisply[model.DealResultAsInt];
+        }
     }
 }
