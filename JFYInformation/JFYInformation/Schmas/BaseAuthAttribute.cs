fd5207365ae11020fc9896faaeb4caa2e44438f7
applenele <1941973283@qq.com>
2015-08-07 14:47:14 +0800

    权限

---
+++ JFYInformation/JFYInformation/Schmas/BaseAuthAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace JFYInformation.Schmas
+{
+    public class BaseAuthAttribute: AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("/Shared/AccessDenied");
+        }
+    }
+}
---
+++ JFYInformation/JFYInformation/Schmas/ValidateSIDAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace JFYInformation.Schmas
+{
+    class ValidateSIDAttribute:BaseAuthAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.QueryString["sid"] != null
+                    && httpContext.Session["sid"].ToString() == httpContext.Request.QueryString["sid"]
+                    || httpContext.Request.Form["sid"] != null
+                    && httpContext.Session["sid"].ToString() == httpContext.Request.Form["sid"].ToString())
+                return true;
+            else
+                return false;
+        }
+    }
+}
